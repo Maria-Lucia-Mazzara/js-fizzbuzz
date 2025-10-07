aa02e97e862c8da7ed5b34dc083a30d446618b2b
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-07 16:31:53 +0200

    sistemata riga 4 perchè prima avevo modulato solo il 5 e ora ho modulato sia il 5 che il 3

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
-for (let i=1; i <= 100; i++) {
-
+for (let i = 1; i <= 100; i++) {
+    // qui prendo i sottomultipli di 5 e 3
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        // qui prendo i sottomultipli di 3
+        console.log("Fizz");
+    } else if (i % 5 == 0){
+         // qui prendo i sottomultipli di 5
+        console.log("Buzz");
+    } else
+    console.log(i);
+        
 }
+        
